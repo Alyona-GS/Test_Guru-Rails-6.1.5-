92eacf056fb21827ebef936d6b273c6c5eaaf4c0
Alyona-GS <asgor1994@gmail.com>
2023-03-29 13:48:03 +0400

    feat: add github_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_year
     Time.now.year
   end
+
+  def github_url (author, project)
+    link_to("TestGuru", "https://github.com/#{author}/#{project}", target: "_blank")
+  end
 end
